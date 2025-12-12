948770298fa82d11bf3f9c3ba5b266c0d53dc1a5
Felipe Etchatz de Almeida <felipe.etchatz@gmail.com>
2025-12-12 11:18:48 -0300

    feat: add routes for shorten, redirect, and analytics endpoints

--- challenges/01-url-shortener/implementation/config/routes.rb
+++ challenges/01-url-shortener/implementation/config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      post "shorten", to: "shorten#create"
+      get "analytics/:short_code", to: "analytics#show", as: :analytics
+    end
+  end
+
+  get ":short_code", to: "redirects#show"
 end
